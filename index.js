a7776790c42c6862f4c931cff787abc2c98b1f39
JanzinMarcelo <cruzconstante@gmail.com>
2023-11-12 15:23:31 -0300

    feats:Listausuarioscadastrados

--- index.js
+++ index.js
@@ -2,7 +2,57 @@ import express from 'express';
 
 const porta = 3000;
 const host ='0.0.0.0';
+var listaUsuarios = [];
 
+function processaCadastroUsuario(requisicao, resposta){
+   const usuario = {
+    nome: requisicao.query.nome,
+    Sobrenome: requisicao.query.Sobrenome,
+    NomeUsuario: requisicao.query.NomeUsuario
+
+   } 
+   listaUsuarios.push(usuario);
+   let conteudoResposta =`
+    <!DOCTYPE html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Menu do sistema</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      </head>
+     <body>
+        <h1> Lista de Usuários Cadastrados </h1>
+        <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Nome de Usuário</th>
+        <tr>
+        </thead>
+        <tbody>`;
+        
+    for(const usuario of listaUsuarios){
+            conteudoResposta+=`
+            <tr>
+                <td>${usuario.nome}</td>
+                <td>${usuario.Sobrenome}</td>
+                <td>${usuario.NomeUsuario}</td>
+            </tr>
+            `;
+        }
+
+        conteudoResposta+=`
+                </tbody>
+            </table>
+            <a class="btn btn-primary" href="/"role="button">Voltar ao menu</a>
+            <a class="btn btn-primary" href="/cadastraUsuario.html"role="button">Continuar Cadastrando</a>
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </html>
+
+        `;
+        resposta.send(conteudoResposta);
+}
 const app = express();
 
 app.use(express.static('./paginas'));
@@ -17,13 +67,17 @@ app.get('/',(requisicao,resposta)=>{
      <body>
         <h1> Menu </h1>
         <ul>
-            <li><a href="/cadastro">Cadastrar Usuário</a></li>
+            <li><a href="/cadastraUsuario.html">Cadastrar Usuário</a></li>
          </ul>
     </body>
      </html>
      `);
 
 })
+
+app.get('/cadastrarUsuario',processaCadastroUsuario);
+    
+
 
 app.listen(porta,host,()=>{
     console.log(`Servidor executado na url http://${host}:${porta}`);   
